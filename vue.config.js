029dfd9c72851d846e1e1c71721405a50fb2d4f0
米花爱吃爆米花 <2524687460@qq.com>
2023-06-01 20:04:34 +0800

    我自己尝试提交

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  // 给路径配置别名  导入css/js/图片文件等
+  configureWebpack: {
+    resolve: {
+      // extensions:[] 这些vue已经默认配置了
+      alias: {
+        // src配置别名为@  vue已经默认配置了
+        assets: "@/assets",
+        common: "@/common",
+        components: "@/components",
+        network: "@/network",
+        views: "@/views",
+      },
+    },
+  },
+});
