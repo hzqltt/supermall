70b474fbd2252ea73176ce8f5c80afecd3da3f72
米花爱吃爆米花 <2524687460@qq.com>
2023-06-04 12:00:31 +0800

    完成了tabControl

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -10,12 +10,19 @@
         <recommend :recommends="recommends"></recommend>
         <!-- 本周推荐 -->
         <feature></feature>
+        <!--流行、新款、精选TabControl  -->
+        <tab-control class="tab-control" :titles="['流行', '新款', '精选']"></tab-control>
+
+        <div style="height:700px"></div>
 
     </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar';
+import TabControl from 'components/content/tabControl/TabControl'
+
+
 import HomeSwiper from './childComps/HomeSwiper.vue';
 import Recommend from './childComps/Recommend.vue';
 import Feature from './childComps/Feature.vue';
@@ -24,13 +31,17 @@ import { getHomeMultidata } from 'network/home'   //default才能省略大括号
 
 
 
+
 export default {
     name: "Home",
     components: {
         NavBar,
+        TabControl,
         HomeSwiper,
         Recommend,
-        Feature
+        Feature,
+        TabControl
+
     },
     data() {
         return {
@@ -76,6 +87,12 @@ export default {
     z-index: 9;
     /* z-index 属性设定了一个定位元素及其后代元素或 flex 项目的 z-order。
     当元素之间重叠的时候，z-index 较大的元素会覆盖较小的元素在上层进行显示。 */
+}
+
+.tab-control {
+    /* 粘性定位元素 移动端适应  这里貌似不适应 */
+    position: sticky;
+    top: 44px;
 }
 </style>
 
